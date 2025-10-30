47bd66de84f32cf7d172760af2e305ef48471560
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2025-10-31 03:35:19 +0530

    fix - tests snapshot usage and package verions

--- tests/acceptance/percy-snapshots-test.js
+++ tests/acceptance/percy-snapshots-test.js
@@ -6,10 +6,17 @@ import percySnapshot from '@percy/ember';
 module('Acceptance | Percy snapshots', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('Dashboard snapshot', async function (assert) {
+  test('Percy snapshot', async function (assert) {
+    await visit('/');
+
+    await percySnapshot(assert);
+
+    assert.ok(true);
+  });
+
+  test('Dashboard renders correctly', async function (assert) {
     await visit('/');
 
     assert.dom('.main-page').exists('Main page component is rendered');
-    await percySnapshot('Dashboard');
   });
 });
--- tests/integration/components/main-page-percy-test.js
+++ tests/integration/components/main-page-percy-test.js
@@ -2,16 +2,22 @@ import { render } from '@ember/test-helpers';
 import { setupRenderingTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 import { hbs } from 'ember-cli-htmlbars';
-import { snapshot } from '@percy/ember';
+import percySnapshot from '@percy/ember';
 
 module('Integration | Component | MainPage Percy snapshots', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('MainPage component snapshot', async function (assert) {
+  test('Percy snapshot', async function (assert) {
+    await render(hbs`<MainPage />`);
+
+    await percySnapshot(assert);
+
+    assert.ok(true);
+  });
+
+  test('MainPage component renders', async function (assert) {
     await render(hbs`<MainPage />`);
 
     assert.dom('.main-page').exists('Main page component is rendered');
-
-    await snapshot('MainPage Component');
   });
 });
